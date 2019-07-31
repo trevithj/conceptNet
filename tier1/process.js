43b0ffdde91de6a42c8d2934f759cb49e7f7ed14
trevithj <trevithj@gmail.com>
2019-07-31 18:22:28 +1200

    Complete processing of json hierarchy tier1

--- tier1/process.js
+++ tier1/process.js
@@ -9,41 +9,64 @@ const getVisitCount = map => node => {
 };
 
 const recursiveDFS = (props) => {
-	const {node, visitCount, leafDepth, edges, depth = 0} = props;
+	const {node, visitCount, leafDepth, noteEdges, depth = 0} = props;
 	if (!node) return; //non-existent. Just in case.
 	if (visitCount(node) > 1) return; //already visited
 
 	const tgts = getTerm(node) || []; //Hmm... need to note nulls?
-	if(tgts.length === 0) {
-		//node is a leaf, so note node and depth
-		leafDepth(node, depth);
-		return;
-	}
+	if(tgts.length === 0) return; //node is a leaf
 	//else carry on searching down each path
+
 	tgts.forEach(tgt => {
 		recursiveDFS({ ...props, node:tgt, depth: depth + 1 });
 		//add to edges as we return up the tree
-		edges.push({ tgt, src:node });
+		noteEdges({ tgt, src:node, depth: depth +1 });
 	});
 }
 
 const examineTree = term => {
+	//note if a node has been visited already during processing
 	const visitCount = getVisitCount({});
+	//find the deepest path in the network
 	const deepest = { node: term, depth: 0 };
-	const leafDepth = (node, depth) => {
-		if(depth < deepest.depth) return;
-		deepest.node = node;
-		deepest.depth = depth;
-	};
-	const edges = [];
-	recursiveDFS({ node:term, visitCount, leafDepth, edges });
-	return { deepest, edges };
+	//create a reverse-mapped network: tgt => [src...]
+	const net = {};
+
+	const noteEdges = ({src, tgt, depth}) => {
+		const sourceList = net[tgt] || [];
+		sourceList.push(src);
+		net[tgt] = sourceList;
+		//note deepest so far
+		if(depth > deepest.depth) {
+			deepest.node = tgt;
+			deepest.depth = depth;
+		};
+	}
+	//search the network
+	recursiveDFS({ node:term, visitCount, noteEdges });
+	return { deepest, net };
 };
 
+
+const recursiveBuildJSON = (json, net, node) => {
+	const children = net[node] || [];
+	children.forEach(child => {
+		json[child] = {};
+		recursiveBuildJSON(json[child], net, child);
+	});
+}
+
 const doProcess = term => {
-	const result = examineTree(term);
-	//todo: form JSON structure
-	return result;
+	const { deepest, net } = examineTree(term);
+	const json = {};
+	if (deepest.depth > 0) {
+		//assume the deepest node is the most general concept
+		const root = deepest.node;
+		json[root] = {};
+		recursiveBuildJSON(json[root], net, deepest.node);
+	}
+	
+	return { deepest, net, json};
 };
 
 module.exports = doProcess;
--- tier1/process.test.js
+++ tier1/process.test.js
@@ -2,17 +2,19 @@ const doProcess = require('./process');
 
 describe('Tier 1 processing', () => {
 
-	it('should', () => {
+	it('should return empty net when no results are found', () => {
 		const result = doProcess('unknown');
-		expect(result.edges).toEqual([]);
+		expect(result.net).toEqual({});
 		expect(result.deepest.depth).toEqual(0);
-		console.log(result);
+		//console.log(result);
 	});
 
-	it('should', () => {
-		const result = doProcess('idea');
-		expect(result.edges.length).toBeGreaterThan(0);
+	it('should return populated net and data', () => {
+		const result = doProcess('saturn');
+		const nodes = Object.keys(result.net);
+		expect(nodes.length).toBeGreaterThan(0);
 		expect(result.deepest.depth).toBeGreaterThan(0);
-		console.log(result);
+		//console.log(result);
+		//console.log(JSON.stringify(result.json, null, 2));
 	});
 });
